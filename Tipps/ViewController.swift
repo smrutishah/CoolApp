1ef57cae728e06395c0375eee5e361afdbd4cdd9
Smruti Shah <smrutishah@smrutis-mbp.fios-router.home>
2017-12-30 19:45:01 -0600

    Initial Commit

---
+++ Tipps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tipps
+//
+//  Created by Smruti Shah on 12/30/17.
+//  Copyright © 2017 Smruti Shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
